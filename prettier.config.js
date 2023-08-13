54661a6cda88ebb11b3e04868bd5ce236b1f8611
Dyvd4 <david.kimmich@gmx.net>
2023-08-13 11:21:36 +0200

    base setup

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	trailingComma: "es5",
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+	singleQuote: false,
+	printWidth: 100,
+	plugins: [import("prettier-plugin-tailwindcss")],
+};
