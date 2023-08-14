38c986d57208d817c9c764e2375c912970c24e7a
Dyvd4 <david.kimmich@gmx.net>
2023-08-14 13:34:02 +0200

    add button

---
+++ src/components/buttons/Button/Button.tsx
@@ -0,0 +1,30 @@
+"use-client";
+
+import { ComponentPropsWithRef, PropsWithChildren } from "react";
+import { cn } from "@/utils/component-utils";
+
+type _ButtonProps = {};
+
+export type ButtonProps = _ButtonProps &
+	Omit<PropsWithChildren<ComponentPropsWithRef<"button">>, keyof _ButtonProps>;
+
+function Button({ className, children, ...props }: ButtonProps) {
+	return (
+		<button
+			className={cn(
+				`bg-secondary hover:text-primary hover:bg-primary
+							rounded-full px-8
+							py-3 text-lg
+							transition-all
+							duration-300
+							hover:shadow-[0px_12px_64px_0px_rgba(96,83,248,0.48)] `,
+				className
+			)}
+			{...props}
+		>
+			{children}
+		</button>
+	);
+}
+
+export default Button;
