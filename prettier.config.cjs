5f4733450650b5e28fd4d3f1fe07c9df9e14fe84
Dyvd4 <david.kimmich@gmx.net>
2023-08-14 12:29:06 +0200

    setup prettier properly

--- prettier.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	trailingComma: "es5",
-	tabWidth: 4,
-	useTabs: true,
-	semi: true,
-	singleQuote: false,
-	printWidth: 100,
-	plugins: [import("prettier-plugin-tailwindcss")],
-};
