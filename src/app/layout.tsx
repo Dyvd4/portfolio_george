8cb23cb45de1a8d3232b89e5f3b19ee30a3ff35d
Dyvd4 <david.kimmich@gmx.net>
2023-08-14 16:59:35 +0200

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 		<html lang="en">
 			<head>
 				<title>Portfolio</title>
-				<link rel="icon" href="/Logo.svg" type="image/x-icon" />
+				<link rel="icon" href="/Favicon.svg" type="image/x-icon" />
 			</head>
 			<body className={matterFont.className}>
 				<Navbar />
