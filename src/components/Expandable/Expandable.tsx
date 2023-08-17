5811b9cf143fa67a06b2b9f3a14441e8c536d0be
Dyvd4 <david.kimmich@gmx.net>
2023-08-17 18:33:59 +0200

    added Expandable

--- src/components/Expandable/Expandable.tsx
+++ src/components/Expandable/Expandable.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { ComponentPropsWithRef, PropsWithChildren, useState } from "react";
-import { cn } from "@/utils/component-utils";
 import ArrowDownCircle from "@/components/icons/ArrowDownCircle";
 import useToggle from "@/hooks/useToggle";
+import { cn } from "@/utils/component-utils";
 import autoAnimate from "@formkit/auto-animate";
 import classNames from "classnames";
+import { ComponentPropsWithRef, PropsWithChildren, useState } from "react";
 
 type _ExpandableProps = {
 	title: string;
@@ -17,31 +17,37 @@ export type ExpandableProps = _ExpandableProps &
 
 function Expandable({ className, children, title, description, ...props }: ExpandableProps) {
 	const [isExpanded, , toggleIsExpanded] = useToggle(false);
+	const [hoverIsActive, setHoverIsActive] = useState(false);
+
 	return (
 		<div
+			onMouseOut={() => setHoverIsActive(false)}
+			onMouseOver={() => setHoverIsActive(true)}
 			ref={(ref) => ref && autoAnimate(ref)}
 			onClick={toggleIsExpanded}
 			className={cn(
-				`border-1 border-tertiary hover:border-primary group
+				`border-1 border-tertiary group
 				flex w-[576px] cursor-pointer flex-col
 				gap-4 rounded-lg border p-6 text-xl`,
-				{},
+				{
+					"border-primary": !!hoverIsActive,
+				},
 				className
 			)}
 			{...props}
 		>
 			<div className="flex items-center justify-between">
-				<h1 className="text-primary">{title}</h1>
+				<h1>{title}</h1>
 				<ArrowDownCircle
-					className={classNames("group-hover:fill-primary", {
+					className={classNames({
 						"rotate-180": isExpanded,
 					})}
-					variant="secondary"
+					isActive={!!hoverIsActive}
 				/>
 			</div>
 			{isExpanded && (
 				<>
-					<p className="color-secondary">{description}</p>
+					<p className="text-secondary">{description}</p>
 				</>
 			)}
 		</div>
