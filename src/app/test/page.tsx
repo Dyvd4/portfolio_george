c5343000a21e61c4c594ca615b27906f15404b9c
Dyvd4 <david.kimmich@gmx.net>
2023-08-18 07:24:08 +0200

    fixed page props error

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -2,19 +2,12 @@ import { ComponentPropsWithRef, PropsWithChildren } from "react";
 import { cn } from "@/utils/component-utils";
 import Expandable from "@/components/Expandable/Expandable";
 
-type _PageProps = {};
-
-export type PageProps = _PageProps &
-	Omit<PropsWithChildren<ComponentPropsWithRef<"div">>, keyof _PageProps>;
-
-function Page({ className, children, ...props }: PageProps) {
+function Page(props) {
 	return (
 		<div
 			className={cn(
-				`absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 transform`,
-				className
+				`absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 transform`
 			)}
-			{...props}
 		>
 			<Expandable title="test" description="kdjsffjsdfjksklflks" />
 		</div>
