e1150a3fbaba1f2e7b4fdcce66de86ddac1359d9
Dyvd4 <david.kimmich@gmx.net>
2023-08-19 19:18:47 +0200

    added hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import ContactSection from "./sections/ContactSection";
 import FaqSection from "./sections/FaqSection";
+import HeroSection from "./sections/HeroSection";
 
 export default function Home() {
 	return (
 		<>
+			<HeroSection />
 			<ContactSection />
 			<FaqSection />
 		</>
--- src/app/sections/ContactSection.tsx
+++ src/app/sections/ContactSection.tsx
@@ -1,15 +1,13 @@
-"use client";
-
-import { ComponentPropsWithRef, PropsWithChildren } from "react";
-import { cn } from "@/utils/component-utils";
 import Button from "@/components/buttons/Button";
+import { cn } from "@/utils/component-utils";
+import { ComponentPropsWithRef } from "react";
 
 type _ContactSectionProps = {};
 
 export type ContactSectionProps = _ContactSectionProps &
-	Omit<PropsWithChildren<ComponentPropsWithRef<"section">>, keyof _ContactSectionProps>;
+	Omit<ComponentPropsWithRef<"section">, keyof _ContactSectionProps>;
 
-function ContactSection({ className, children, ...props }: ContactSectionProps) {
+function ContactSection({ className, ...props }: ContactSectionProps) {
 	return (
 		<section className={cn(`relative min-h-screen`, className)} {...props}>
 			<div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-center gap-8">
