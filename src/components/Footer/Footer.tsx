5bf2cd4cdc6a7a1390ca07055dde807c4dc8f0fb
Dyvd4 <david.kimmich@gmx.net>
2023-08-20 10:53:52 +0200

    added dev sign to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,6 +2,7 @@ import Button from "@/components/buttons/Button";
 import { Behance, Dribble, Telegram } from "@/components/icons";
 import { cn } from "@/utils/component-utils";
 import { ComponentPropsWithRef, PropsWithChildren } from "react";
+import DevSign from "@/components/DevSign";
 
 type _FooterProps = {};
 
@@ -11,8 +12,10 @@ export type FooterProps = _FooterProps &
 function Footer({ className, children, ...props }: FooterProps) {
 	return (
 		<footer className={cn(`flex flex-col justify-center gap-[360px]`, className)} {...props}>
-			<div className="flex flex-col items-center gap-8">
-				<h1 className="text-primary text-8xl">hello@itsgeorge.com</h1>
+			<div className="flex flex-col items-center gap-6">
+				<h1 className="text-primary text-[88px] leading-[150%] underline">
+					hello@itsgeorge.com
+				</h1>
 				<Button>Contact form</Button>
 			</div>
 			<div className="flex justify-between px-12 py-8">
@@ -21,6 +24,7 @@ function Footer({ className, children, ...props }: FooterProps) {
 					<Behance variant="secondary" />
 					<Dribble variant="secondary" />
 				</div>
+				<DevSign />
 				<div className="text-secondary text-sm">
 					© 2023 George Palkin. All rights reserved.
 				</div>
