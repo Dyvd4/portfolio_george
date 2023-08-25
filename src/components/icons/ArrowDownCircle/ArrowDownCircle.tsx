8c1373aaeaa8d60f5a9dc0eda4a6a5236abea8d8
Dyvd4 <david.kimmich@gmx.net>
2023-08-25 19:13:59 +0200

    removed global icon styles and added group hover to icons

--- src/components/icons/ArrowDownCircle/ArrowDownCircle.tsx
+++ src/components/icons/ArrowDownCircle/ArrowDownCircle.tsx
@@ -14,10 +14,11 @@ function ArrowDownCircle({ className, ...props }: ArrowDownCircleProps) {
 			viewBox="0 0 24 24"
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
-			className={cn("icon", className)}
+			className={cn("group", className)}
 			{...props}
 		>
 			<path
+				className="group-hover:stroke-primary stroke-secondary cursor-pointer"
 				fillRule="evenodd"
 				clipRule="evenodd"
 				d="M2.75024 12C2.75024 17.108 6.89124 21.25 12.0002 21.25C17.1082 21.25 21.2502 17.108 21.2502 12C21.2502 6.892 17.1082 2.75 12.0002 2.75C6.89124 2.75 2.75024 6.892 2.75024 12Z"
@@ -26,6 +27,7 @@ function ArrowDownCircle({ className, ...props }: ArrowDownCircleProps) {
 				strokeLinejoin="round"
 			/>
 			<path
+				className="group-hover:stroke-primary stroke-secondary cursor-pointer"
 				d="M8.52905 10.5577L12.0001 14.0437L15.4711 10.5577"
 				strokeWidth="1.5"
 				strokeLinecap="round"
--- src/components/icons/ArrowRight/ArrowRight.tsx
+++ src/components/icons/ArrowRight/ArrowRight.tsx
@@ -14,16 +14,18 @@ function ArrowRight({ className, ...props }: ArrowRightProps) {
 			viewBox="0 0 32 32"
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
-			className={cn("icon", className)}
+			className={cn("group", className)}
 			{...props}
 		>
 			<path
+				className="group-hover:stroke-primary stroke-secondary cursor-pointer"
 				d="M26.3334 15.6343H6.33337"
 				strokeWidth={2}
 				strokeLinecap="round"
 				strokeLinejoin="round"
 			/>
 			<path
+				className="group-hover:stroke-primary stroke-secondary cursor-pointer"
 				d="M18.267 7.60172L26.3336 15.6337L18.267 23.6671"
 				strokeWidth={2}
 				strokeLinecap="round"
