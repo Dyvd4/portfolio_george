0ceb332bfa00aaeb1a6b5961f72f39c931cc62e8
Dyvd4 <david.kimmich@gmx.net>
2023-09-06 12:37:41 +0200

    try fix

--- src/app/config.ts
+++ src/app/config.ts
@@ -9,4 +9,11 @@ const envVariables = z.object({
 	CONTACT_RECIPIENT: z.string(),
 });
 
-export default envVariables.parse(process.env);
+export default envVariables.parse({
+	BASE_URL: process.env.BASE_URL!,
+	NEXT_PUBLIC_DEV_SIGN_URL: process.env.NEXT_PUBLIC_DEV_SIGN_URL!,
+	SMTP_HOST: process.env.SMTP_HOST!,
+	SMPT_USERNAME: process.env.SMPT_USERNAME!,
+	SMPT_PASSWORD: process.env.SMPT_PASSWORD!,
+	CONTACT_RECIPIENT: process.env.CONTACT_RECIPIENT!,
+});
