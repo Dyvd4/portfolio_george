c2909cb762db469c8423965393d483d3ca0c3711
Dyvd4 <david.kimmich@gmx.net>
2023-09-10 13:12:03 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 	const router = useRouter();
 
 	useEffect(() => {
-		router.push(window.location.hash);
+		if (footerIsActive) router.push(window.location.hash);
 	}, [footerIsActive]);
 
 	return (
