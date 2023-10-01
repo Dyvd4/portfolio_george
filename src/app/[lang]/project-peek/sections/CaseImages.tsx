b6051239ebc5b8fce95a5336c49f27d1cf702f9d
Dyvd4 <david.kimmich@gmx.net>
2023-10-01 11:54:00 +0200

    added video player to project-peek

--- src/app/[lang]/project-peek/sections/CaseImages.tsx
+++ src/app/[lang]/project-peek/sections/CaseImages.tsx
@@ -1,7 +1,11 @@
+import modalIsActiveAtom from "@/atoms/modalIsActiveAtom";
 import CaseImage, { IphoneCaseImage } from "@/components/CaseImage";
+import VideoPlayerModal from "@/components/VideoPlayerModal";
 import CaseImages from "@/components/sections/CaseImagesSection";
 import IphoneCaseSection from "@/components/sections/IphoneCaseSection";
+import useFollowingCursorButton from "@/hooks/useFollowingCursorButton";
 import PopupImg from "@/public/project-peek/case-images/Pop-up.png";
+import CaseVideoWithoutButtonImg from "@/public/project-peek/case-images/case-video-without-button.png";
 import CaseVideoImg from "@/public/project-peek/case-images/case-video.svg";
 import ChooseYourPeekImg from "@/public/project-peek/case-images/choose-your-peek.png";
 import IphoneBePreparedForTheFutureImg from "@/public/project-peek/case-images/iphone-be-prepared-for-the-future.png";
@@ -14,108 +18,135 @@ import PeekMenuImg from "@/public/project-peek/case-images/peek-menu.png";
 import PickAnyColorImg from "@/public/project-peek/case-images/pick-any-color.png";
 import SpeakerImg from "@/public/project-peek/case-images/speaker.png";
 import YourCartImg from "@/public/project-peek/case-images/your-cart.png";
+import { useAtom } from "jotai";
+import { useRef } from "react";
 
 function PeekCaseImages() {
+	const caseVideoImgRef = useRef<HTMLElement | null>(null);
+	const [modalIsActive, setModalIsActive] = useAtom(modalIsActiveAtom);
+	useFollowingCursorButton({
+		parentRef: caseVideoImgRef,
+		buttonProps: {
+			title: "Play video",
+			onClick: () => {
+				setModalIsActive(true);
+			},
+		},
+	});
 	return (
-		<CaseImages>
-			<section>
-				<CaseImage
-					className="p-0"
-					imageProps={{
-						src: CaseVideoImg,
-						alt: "Case video",
-					}}
-				/>
-			</section>
-			<section>
-				<CaseImage
-					imageProps={{
-						src: PickAnyColorImg,
-						alt: "Pick any color with the app",
-					}}
-				/>
-			</section>
-			<IphoneCaseSection>
-				<IphoneCaseImage
-					imageProps={{
-						src: IphoneBePreparedForTheFutureImg,
-						alt: "Iphone be prepared for the future",
-					}}
-				/>
-				<IphoneCaseImage
-					imageProps={{
-						src: IphonePickAnyColorImg,
-						alt: "Iphone pick any color",
-					}}
-				/>
-			</IphoneCaseSection>
-			<section>
-				<CaseImage
-					imageProps={{
-						src: ChooseYourPeekImg,
-						alt: "Choose your peek",
-					}}
-				/>
-			</section>
-			<IphoneCaseSection>
-				<IphoneCaseImage
-					imageProps={{
-						src: IphoneChooseYourPeekImg,
-						alt: "Iphone choose your peek",
-					}}
-				/>
-				<IphoneCaseImage
-					imageProps={{
-						src: IphoneChooseYourPeek2Img,
-						alt: "Iphone choose your peek part 2",
-					}}
-				/>
-			</IphoneCaseSection>
-			<section>
-				<CaseImage
-					imageProps={{
-						src: PeekMenuImg,
-						alt: "Peek menu",
-					}}
-				/>
-			</section>
-			<IphoneCaseSection>
-				<IphoneCaseImage
-					imageProps={{
-						src: SpeakerImg,
-						alt: "Speaker",
-					}}
-				/>
-				<IphoneCaseImage
-					imageProps={{
-						src: IphoneMenuImg,
-						alt: "Iphone peek menu",
-					}}
-				/>
-			</IphoneCaseSection>
-			<section>
-				<CaseImage
-					imageProps={{
-						src: YourCartImg,
-						alt: "Your cart",
-					}}
-				/>
-			</section>
-			<IphoneCaseSection>
-				<IphoneCaseImage
-					imageProps={{
-						src: IphonePeekSpeakerAndInformationImg,
-						alt: "Iphone peek speaker and information",
-					}}
-				/>
-				<IphoneCaseImage
-					imageProps={{
-						src: PopupImg,
-						alt: "Pop up",
-					}}
-				/>
-			</IphoneCaseSection>
-		</CaseImages>
+		<>
+			<CaseImages>
+				<section ref={caseVideoImgRef}>
+					<CaseImage
+						className="p-0 sm:hidden"
+						imageProps={{
+							src: CaseVideoImg,
+							alt: "Case video",
+						}}
+					/>
+					<CaseImage
+						className="hidden p-0 sm:block"
+						imageProps={{
+							src: CaseVideoWithoutButtonImg,
+							alt: "Case video",
+						}}
+					/>
+				</section>
+				<section>
+					<CaseImage
+						imageProps={{
+							src: PickAnyColorImg,
+							alt: "Pick any color with the app",
+						}}
+					/>
+				</section>
+				<IphoneCaseSection>
+					<IphoneCaseImage
+						imageProps={{
+							src: IphoneBePreparedForTheFutureImg,
+							alt: "Iphone be prepared for the future",
+						}}
+					/>
+					<IphoneCaseImage
+						imageProps={{
+							src: IphonePickAnyColorImg,
+							alt: "Iphone pick any color",
+						}}
+					/>
+				</IphoneCaseSection>
+				<section>
+					<CaseImage
+						imageProps={{
+							src: ChooseYourPeekImg,
+							alt: "Choose your peek",
+						}}
+					/>
+				</section>
+				<IphoneCaseSection>
+					<IphoneCaseImage
+						imageProps={{
+							src: IphoneChooseYourPeekImg,
+							alt: "Iphone choose your peek",
+						}}
+					/>
+					<IphoneCaseImage
+						imageProps={{
+							src: IphoneChooseYourPeek2Img,
+							alt: "Iphone choose your peek part 2",
+						}}
+					/>
+				</IphoneCaseSection>
+				<section>
+					<CaseImage
+						imageProps={{
+							src: PeekMenuImg,
+							alt: "Peek menu",
+						}}
+					/>
+				</section>
+				<IphoneCaseSection>
+					<IphoneCaseImage
+						imageProps={{
+							src: SpeakerImg,
+							alt: "Speaker",
+						}}
+					/>
+					<IphoneCaseImage
+						imageProps={{
+							src: IphoneMenuImg,
+							alt: "Iphone peek menu",
+						}}
+					/>
+				</IphoneCaseSection>
+				<section>
+					<CaseImage
+						imageProps={{
+							src: YourCartImg,
+							alt: "Your cart",
+						}}
+					/>
+				</section>
+				<IphoneCaseSection>
+					<IphoneCaseImage
+						imageProps={{
+							src: IphonePeekSpeakerAndInformationImg,
+							alt: "Iphone peek speaker and information",
+						}}
+					/>
+					<IphoneCaseImage
+						imageProps={{
+							src: PopupImg,
+							alt: "Pop up",
+						}}
+					/>
+				</IphoneCaseSection>
+			</CaseImages>
+			<VideoPlayerModal
+				url={"../../../../../case-video.mp4"}
+				controls
+				playing={modalIsActive}
+			/>
+		</>
 	);
 }
 
