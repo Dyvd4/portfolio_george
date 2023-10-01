5c1cce1f651907faaaf0e414c4fb8db3ce2ef1db
Dyvd4 <david.kimmich@gmx.net>
2023-10-01 10:58:48 +0200

    added VideoPlayerModal-component

---
+++ src/components/VideoPlayerModal/VideoPlayerModal.tsx
@@ -0,0 +1,18 @@
+import dynamic from "next/dynamic";
+import type { ReactPlayerProps } from "react-player";
+import Modal from "../Modal";
+const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
+
+function VideoPlayerModal(props: ReactPlayerProps) {
+	return (
+		<Modal
+			useCloseIcon
+			className={`max-h-[184px] max-w-[328px] overflow-hidden p-0 
+				sm:max-h-none sm:max-w-none 2xl:max-h-[500px] 2xl:max-w-[888px]`}
+		>
+			<ReactPlayer width={"100%"} height={"100%"} {...props} />
+		</Modal>
+	);
+}
+
+export default VideoPlayerModal;
