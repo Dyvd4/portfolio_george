c5cf3dbae5f6e168aab46ab50618016f2362c42a
Dyvd4 <david.kimmich@gmx.net>
2023-10-01 11:02:20 +0200

    Update VideoPlayerModal.tsx

--- src/components/VideoPlayerModal/VideoPlayerModal.tsx
+++ src/components/VideoPlayerModal/VideoPlayerModal.tsx
@@ -3,14 +3,14 @@ import type { ReactPlayerProps } from "react-player";
 import Modal from "../Modal";
 const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
-function VideoPlayerModal(props: ReactPlayerProps) {
+function VideoPlayerModal({ playerProps }: { playerProps: ReactPlayerProps }) {
 	return (
 		<Modal
 			useCloseIcon
 			className={`max-h-[184px] max-w-[328px] overflow-hidden p-0 
 				sm:max-h-none sm:max-w-none 2xl:max-h-[500px] 2xl:max-w-[888px]`}
 		>
-			<ReactPlayer width={"100%"} height={"100%"} {...props} />
+			<ReactPlayer width={"100%"} height={"100%"} {...playerProps} />
 		</Modal>
 	);
 }
